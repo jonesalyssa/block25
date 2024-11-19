368ee005213794b355e6ff441ace685a6402d71a
jonesalyssa <jonesalyssan@gmail.com>
2024-11-19 02:53:54 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./index.css";
+
+const Color = ({ color, setSelectedColor }) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+};
+
+const Picker = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
+  return (
+    <div>
+      <div id="navbar">
+        <div>Currently selected:</div>
+        <div className={selectedColor}>{selectedColor}</div>
+      </div>
+      <div id="colors-list">
+        <Color color="yellow" setSelectedColor={setSelectedColor} />
+        <Color color="violet" setSelectedColor={setSelectedColor} />
+        <Color color="blue" setSelectedColor={setSelectedColor} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
